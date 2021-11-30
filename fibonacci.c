3008b751e3db0413e9546854bd614aabad497f7d
Yasir Aalam <94714966+Yasiraalam@users.noreply.github.com>
2021-11-30 23:20:48 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,25 @@
+//use recursion to print nth number of the fibonacci series:
+//   0 1  1 2 35 8 13 21 34.....
+
+#include<stdio.h>
+#include<math.h>
+int fib(int);
+
+ int main()
+ {
+     int n,fib;
+     print("enter value of n");
+     scanf("%d", &n);
+
+     printf("%d", fib(n-1));
+
+ }
+ int fib(int n)
+ {
+     if (n==0)
+     return ;
+     else if(n==1)
+     return 1;
+     else 
+         return   fib(n-1)+ fib(n-2);
+ }
